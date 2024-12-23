b3a428571f31348d06e4ef2f8180d4343821e1a4
Nguyen Gia Thuy <nguyengiathuy1998@gmail.com>
2024-12-23 14:14:31 +0700

    ATAC_Create core libs for test framework

---
+++ TestCore/Exceptions/PostconditionException.cs
@@ -0,0 +1,19 @@
+namespace TestCore.Exceptions;
+
+public class PostconditionException : Exception
+{
+    public (string Message, string? InnerException) Details { get; }
+
+    public PostconditionException(string message, Exception? innerException = null)
+        : base(message)
+    {
+        Details = (message, innerException?.ToString());
+    }
+
+    public override string ToString()
+    {
+        return base.ToString()
+            + (Details.Message != null ? $", Message: {Details.Message}" : "")
+            + (Details.InnerException != null ? $", InnerException: {Details.InnerException}" : "");
+    }
+}
