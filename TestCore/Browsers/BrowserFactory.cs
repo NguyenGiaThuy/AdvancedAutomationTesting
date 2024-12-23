e88f89e337af0301e4ed70919cc290f7ca6abf7b
Nguyen Gia Thuy <nguyengiathuy1998@gmail.com>
2024-12-23 16:29:18 +0700

    ATAC_Refactore BrowserFactory for cleaner code

--- TestCore/Browsers/BrowserFactory.cs
+++ TestCore/Browsers/BrowserFactory.cs
@@ -4,27 +4,25 @@ public static class BrowserFactory
 {
     public static IBrowser MakeBrowser(string launchBrowser, int implicitTimeout)
     {
-        IDriverConfig driverConfig;
         IWebDriver webDriver;
         switch (launchBrowser)
         {
             case "Firefox":
-                driverConfig = new FirefoxConfig();
+                new WebDriverManager.DriverManager().SetUpDriver(new FirefoxConfig());
                 webDriver = new FirefoxDriver();
                 break;
             case "Chrome":
-                driverConfig = new ChromeConfig();
+                new WebDriverManager.DriverManager().SetUpDriver(new ChromeConfig());
                 webDriver = new ChromeDriver();
                 break;
             case "Edge":
-                driverConfig = new EdgeConfig();
+                new WebDriverManager.DriverManager().SetUpDriver(new EdgeConfig());
                 webDriver = new EdgeDriver();
                 break;
             default:
                 throw new WebDriverException("Invalid browser configuration");
         }
 
-        new WebDriverManager.DriverManager().SetUpDriver(driverConfig);
         webDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromMilliseconds(implicitTimeout);
         IBrowser browser = new Browser(webDriver);
 
