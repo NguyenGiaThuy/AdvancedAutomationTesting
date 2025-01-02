bb2a496a1bfa4c0411366ae087751c7833989256
Nguyen Gia Thuy <nguyengiathuy1998@gmail.com>
2025-01-02 14:57:10 +0700

    ATAC_Fix typos in TestCore

--- TestCore/Apis/ApiFactory.cs
+++ TestCore/Apis/ApiFactory.cs
@@ -2,7 +2,7 @@ namespace TestCore.Apis;
 
 public static class ApiFactory
 {
-    public static IApiClient MakeApi(string baseUrl)
+    public static IApiClient MakeApiClient(string baseUrl)
     {
         return new ApiClient(baseUrl);
     }
